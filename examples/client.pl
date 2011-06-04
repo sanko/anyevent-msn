2d8618f3ebb3a56eebb7c5944a911f432dae7c2c
Sanko Robinson <sanko@cpan.org>
2011-06-04 14:55:42 -0400

    Bringing example up to date with new API

--- examples/client.pl
+++ examples/client.pl
@@ -3,7 +3,29 @@ use strict;
 use warnings;
 use AnyEvent;
 use AnyEvent::MSN;
+use 5.012;
 $|++;
-AnyEvent::MSN->new(username => 'msn@hotmail.com',
-                   password => 'password'
-)->connect->recv;
+my ($user, $pass) = @ARGV;    # XXX - Better to use a GetOpt-like module
+my $msn;
+$msn = AnyEvent::MSN->new(
+    passport => $user, # XXX - I may change the name of this arg before pause
+    password => $pass,
+
+    # Extra user info
+    status          => 'AWY',
+    friendlyname    => 'Just another MSN hacker,',
+    personalmessage => 'This can\'t be life!',
+
+    # Basic events
+    on_im => sub {            # Simple Ping/Pong bot
+        my ($head, $body) = @_;
+        printf qq'%-40s> %s\n', $head->{From}, $body;
+        $msn->im($head->{From}, 'Pong!') if $body eq 'Ping?';
+    },
+    on_nudge => sub {
+        my $head = shift;
+        warn $head->{From} . ' just nudged us';
+        $msn->nudge($head->{From});
+    },
+);
+AnyEvent->condvar->recv;
