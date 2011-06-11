e0e1d3cc883efbe582c4daf722210d3569a481b0
Sanko Robinson <sanko@cpan.org>
2011-06-11 14:20:30 -0400

    New method add_buddy( ... ) and improve set_status( ... )

--- lib/AnyEvent/MSN.pm
+++ lib/AnyEvent/MSN.pm
@@ -970,8 +970,49 @@ XML
             $to, $s->passport, $s->guid;
         $s->send(qq'SDG 0 %d\r\n%s', length($data), $data);
     }
+    sub add_buddy{
+        my $s = shift;
 
-    # Non-OOP utility functions
+           my      $data = sprintf <<'', reverse split '@', shift, 2;
+<ml>
+    <d n="%s">
+        <c n="%s" t="1">
+            <s l="3" n="IM" />
+            <s l="3" n="PE" />
+            <s l="3" n="PF" />
+        </c>
+    </d>
+</ml>
+
+                   $s->send("ADL %d %d\r\n%s", $s->tid, length($data), $data);
+
+
+
+
+        }
+        after set_status => sub {
+            my($s,$status)=@_;
+
+
+                      my $body = sprintf '<user>' . '<s n="PE">
+            <UserTileLocation>0</UserTileLocation><FriendlyName>%s</FriendlyName><PSM>%s</PSM><RUM></RUM><RLT>0</RLT></s>'
+                        . '<s n="IM"><Status>%s</Status><CurrentMedia></CurrentMedia></s>'
+                        . '<sep n="PD"><ClientType>1</ClientType><EpName>%s</EpName><Idle>false</Idle><State>%s</State></sep>'
+                        . '<sep n="PE" epid="%s"><VER>MSNMSGR:15.4.3508.1109</VER><TYP>1</TYP><Capabilities>2952790016:557056</Capabilities></sep>'
+                        . '<sep n="IM"><Capabilities>2953838624:132096</Capabilities></sep>'
+                        . '</user>', __html_escape($s->friendlyname),
+                        __html_escape($s->personalmessage),
+                        $status,
+                        __html_escape($s->location), $status, $s->guid;
+                    my $out =
+                        sprintf
+                        qq[To: 1:%s\r\nRouting: 1.0\r\nFrom: 1:%s;epid=%s\r\n\r\nStream: 1\r\nFlags: ACK\r\nReliability: 1.0\r\n\r\nContent-Length: %d\r\nContent-Type: application/user+xml\r\nPublication: 1.0\r\nUri: /user\r\n\r\n%s],
+                        $s->passport,
+                        $s->passport, $s->guid, length($body), $body;
+                    $s->send("PUT %d %d\r\n%s", $s->tid, length($out), $out);
+
+};
+# Non-OOP utility functions
     sub __html_escape {
         my $x = shift;
         $x =~ s[&][&amp;]sg;
